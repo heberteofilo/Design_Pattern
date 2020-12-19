9f9a9c729edadd4a7322c8165ab3e1ca872bde54
heberteofilo <heberteofilo123@gmail.com>
2020-12-19 15:21:53 -0300

    att

--- Infrastructure/2 - Repositorio/2.2 - Repositorios/RepositorioProduto.cs
+++
@@ -1,9 +0,0 @@
-﻿using Application._2___Interfaces;
-using Application._4___Entidades;
-using Infrastructure._2___Repositorio._2._1___Generic;
-
-namespace Infrastructure._2___Repositorio._2._2___Repositorios
-{
-    class RepositorioProduto : RepositorioGenerico<Product>, IProduct
-    {}
-}
